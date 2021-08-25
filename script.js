3dfcd77edb546fa7ab6a40ef3c3eb072f29138c3
LoloProd <lorenzosacerdote@gmail.com>
2021-08-25 17:16:04 -0300

    Añadir botón de play, cambios varios al js

--- script.js
+++ script.js
@@ -9,25 +9,15 @@ const squares = [
     botRight = document.getElementById("botRight"),
 ]
 
-addNumToArray();
-
-// le damos un eventlistener a cada cuadrado que devuelve su numero
-for (let i = 0; i < squares.length; i++) {
-    squares[i].addEventListener('click', function e() {
-        userInputArray.push(i + 1);
-        onSquareClick();
-    });
-}
-
-
-// función que pasa despues de completar la secuencia
+const playButton = document.getElementById("playButton");
+// Función que pasa despues de completar la secuencia
 function addNumToArray() {
     randomNum = Math.floor(Math.random() * 4) + 1;
     randomOrderArray.push(randomNum);
     console.log('Order is ' + randomOrderArray)
 }
 
-// función que se fija que los dos arrays sean iguales 
+// Función que se fija que los dos arrays sean iguales 
 function checkArrayEquality() {
     for (let i = 0; i < userInputArray.length; i++) {
         if (userInputArray[i] != randomOrderArray[i]) {
@@ -37,16 +27,33 @@ function checkArrayEquality() {
     return true;
 }
 
-// función que se activa cuando apretas un cuadrado 
+// Función que se activa cuando apretas un cuadrado 
 function onSquareClick() {
+    // Reinciar si perdiste
     if (!checkArrayEquality()) {
         console.log('You lost!!');
         randomOrderArray = [];
         userInputArray = [];
         addNumToArray();
         return;
+        // Si no perdiste alargar la secuencia y reniciar el orden introducido por el usuario
     } else if (userInputArray.length == randomOrderArray.length) {
         addNumToArray();
         userInputArray = [];
     }
 }
+
+// ONLOAD
+// Le damos un eventlistener a cada cuadrado para que cuando sean clickeados devuelvan el número que fue clickeado
+for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener('click', function e() {
+        userInputArray.push(i + 1);
+        onSquareClick();
+    });
+}
+playButton.addEventListener('click', function e() {
+    console.log('I work!');
+    // PRÓXIMOS PASOS: CREAR FUNCIÓN QUE SE LLAME ONPLAY QUE LE SUBA EL BRILLO A LOS CUADRADOS EN ORDEN
+});
+// Empezamos el juego llamando esta función
+addNumToArray();
