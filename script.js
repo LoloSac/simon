efc286e3d92b99599ba2de28000427d6ceeecfdd
LoloProd <lorenzosacerdote@gmail.com>
2021-08-26 15:41:29 -0300

    TODO, ME OLVIDE DE COMMITEAR XD

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ const playButton = document.getElementById("playButton");
 function addNumToArray() {
     let randomNum = Math.ceil(Math.random() * 4);
     randomOrderArray.push(randomNum);
-    alert('Order is ' + randomOrderArray)
 }
 
 // Función que se fija que los dos arrays sean iguales 
@@ -34,24 +33,45 @@ function onSquareClick() {
         randomOrderArray = [];
         userInputArray = [];
         addNumToArray();
+        doColorSequence();
         // Si no perdiste alargar la secuencia y reniciar el orden introducido por el usuario
     } else if (userInputArray.length == randomOrderArray.length) {
         addNumToArray();
+        doColorSequence();
         userInputArray = [];
     }
 }
 
+function doColorSequence() {
+    for (let i = 0; i < randomOrderArray.length; i++) {
+        setTimeout(() => {
+            //Reproducimos animación 
+            squares[randomOrderArray[i] - 1].style.animation = "squares .75s";
+            //Una vez que termina la animación le ponemos la default de nuevo
+            setTimeout(() => {
+                squares[randomOrderArray[i] - 1].style.animation = "idle 0s";
+            }, 750);
+        }, 1000 * (i + 1));
+    }
+}
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ONLOAD
 // Le damos un eventlistener a cada cuadrado para que cuando sean clickeados devuelvan el número que fue clickeado
 for (let i = 0; i < squares.length; i++) {
     squares[i].addEventListener('click', function e() {
         userInputArray.push(i + 1);
+        squares[i].style.animation = "squares .5s"
+        setTimeout(() => {
+            squares[i].style.animation = "idle 0s";
+        }, 500);
         onSquareClick();
     });
 }
 playButton.addEventListener('click', function e() {
-    console.log('I work!');
-    // PRÓXIMOS PASOS: CREAR FUNCIÓN QUE SE LLAME ONPLAY QUE LE SUBA EL BRILLO A LOS CUADRADOS EN ORDEN
+    doColorSequence();
 });
-// Empezamos el juego llamando esta función
+// Empezamos el juego llamando estas funciónes
 addNumToArray();
+doColorSequence();
