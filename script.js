177909f0fdef583855e22533843ea76711d18c46
LoloProd <lorenzosacerdote@gmail.com>
2021-09-09 14:14:48 -0300

    cambios fucking minimos

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ const squares = [
     topLeft = document.getElementById("topLeft"),
     topRight = document.getElementById("topRight"),
     botLeft = document.getElementById("botLeft"),
-    botRight = document.getElementById("botRight"),
-]
+    botRight = document.getElementById("botRight")
+];
 
 const playButton = document.getElementById("playButton");
 // Función que pasa despues de completar la secuencia
@@ -14,8 +14,7 @@ function addNumToArray() {
     let randomNum = Math.ceil(Math.random() * 4);
     randomOrderArray.push(randomNum);
 }
-
-// Función que se fija que los dos arrays sean iguales 
+// Función que se fija que los dos arrays sean iguales
 function checkArrayEquality() {
     for (let i = 0; i < userInputArray.length; i++) {
         if (userInputArray[i] != randomOrderArray[i]) {
@@ -25,7 +24,7 @@ function checkArrayEquality() {
     return true;
 }
 
-// Función que se activa cuando apretas un cuadrado 
+// Función que se activa cuando apretas un cuadrado
 function onSquareClick() {
     // Reinciar si perdiste
     if (!checkArrayEquality()) {
@@ -45,7 +44,7 @@ function onSquareClick() {
 function doColorSequence() {
     for (let i = 0; i < randomOrderArray.length; i++) {
         setTimeout(() => {
-            //Reproducimos animación 
+            //Reproducimos animación
             squares[randomOrderArray[i] - 1].style.animation = "squares .5s";
             //Una vez que termina la animación le ponemos la default de nuevo
             setTimeout(() => {
@@ -55,14 +54,13 @@ function doColorSequence() {
     }
 }
 
-
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // ONLOAD
 // Le damos un eventlistener a cada cuadrado para que cuando sean clickeados devuelvan el número que fue clickeado
 for (let i = 0; i < squares.length; i++) {
     squares[i].addEventListener('click', function e() {
         userInputArray.push(i + 1);
-        squares[i].style.animation = "squares .25s"
+        squares[i].style.animation = 'squares .25s';
         setTimeout(() => {
             squares[i].style.animation = "idle 0s";
         }, 250);
